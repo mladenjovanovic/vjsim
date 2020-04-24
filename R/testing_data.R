7e24df96ac25960e52c91cdd77352d48bd2f1fa1
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2020-04-24 08:48:42 +0200

    Update documentation

--- R/testing_data.R
+++ R/testing_data.R
@@ -4,12 +4,13 @@
 #'     Jump mat has a measurement error, with systematic bias of 0, proportional bias of 1
 #'     and random error normally distributed with SD of 0.01s
 #'
-#' \describe{
-#'    \item{athlete}{Character string}
-#'    \item{bodyweight}{Bodyweight in kilograms}
-#'    \item{push_off_distance}{Push-off distance during squat jump in meters}
-#'    \item{external_load}{External load added to bodyweight in kilograms}
-#'    \item{aerial_time}{Aerial time measured with jump mat in seconds}
-#' }
+#' @format A data frame with 24 rows and 5 variables:
+#'   \describe{
+#'      \item{athlete}{Character string}
+#'      \item{bodyweight}{Bodyweight in kilograms}
+#'      \item{push_off_distance}{Push-off distance during squat jump in meters}
+#'      \item{external_load}{External load added to bodyweight in kilograms}
+#'      \item{aerial_time}{Aerial time measured with jump mat in seconds}
+#'   }
 #' @usage data(testing_data)
 "testing_data"
